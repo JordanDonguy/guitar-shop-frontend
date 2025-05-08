1958bab17b1e590523f1de97757cb0b644ca9681
Jordan <jordan.donguy@gmail.com>
2025-05-08 19:57:41 +0200

    Install Tailwind CSS and configure it

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+export default {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+      ],
+    theme: {
+      extend: {
+        fontFamily: {
+          onest: ['Onest', 'sans-serif'],
+        },
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
