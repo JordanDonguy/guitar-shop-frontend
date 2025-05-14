c3f58b7a48e9f6959bb460cb3dbc50d80a862cab
Jordan <jordan.donguy@gmail.com>
2025-05-14 22:53:11 +0200

    Make SingleProduct page and AddToCart component responsive

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -2,70 +2,79 @@ import { useEffect } from "react";
 import { BASE_URL } from "./utils/api";
 import { useAuth } from "./utils/AuthContext";
 import addToCart from "../assets/img/add-to-cart.png";
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-export default function AddToCart({ product_id, brand, name, image_url, price }) {
-    const { user } = useAuth();
+export default function AddToCart({
+  product_id,
+  brand,
+  name,
+  image_url,
+  price,
+}) {
+  const { user } = useAuth();
 
-    async function handleSubmit(event) {
-        event.preventDefault();
+  async function handleSubmit(event) {
+    event.preventDefault();
 
-        if (user) {
-            try {
-                const response = await fetch(`${BASE_URL}/cart/add`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({ product_id, user }),
-                });
-                const result = await response.json();
-                toast.success('Item added to cart!', {
-                    position: "bottom-center",
-                    autoClose: 2000,
-                });
-            } catch (err) {
-                console.error('Error adding item to cart:', err);
-            }
+    if (user) {
+      try {
+        const response = await fetch(`${BASE_URL}/cart/add`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ product_id, user }),
+        });
+        const result = await response.json();
+        toast.success("Item added to cart!", {
+          position: "bottom-center",
+          autoClose: 2000,
+        });
+      } catch (err) {
+        console.error("Error adding item to cart:", err);
+      }
+    } else {
+      try {
+        let cart = JSON.parse(localStorage.getItem("cart")) || [];
+        const index = cart.findIndex((item) => item.product_id === product_id);
+
+        if (index !== -1) {
+          cart[index].quantity += 1;
         } else {
-            try {
-                let cart = JSON.parse(localStorage.getItem('cart')) || [];
-                const index = cart.findIndex(item => item.product_id === product_id);
-
-                if (index !== -1) {
-                    cart[index].quantity += 1;
-                } else {
-                    // Add a new product structure (add other fields as needed)
-                    cart.push({
-                        product_id,
-                        quantity: 1,
-                        brand,
-                        name,
-                        image_url,
-                        price
-                        // optionally include: name, image_url, price, etc.
-                    });
-                }
-
-                localStorage.setItem('cart', JSON.stringify(cart));
-                toast('Item added to cart!', {
-                    position: "bottom-center",
-                    autoClose: 2000,
-                });
-            } catch (err) {
-                console.error('Error adding to local cart:', err);
-            }
+          // Add a new product structure (add other fields as needed)
+          cart.push({
+            product_id,
+            quantity: 1,
+            brand,
+            name,
+            image_url,
+            price,
+            // optionally include: name, image_url, price, etc.
+          });
         }
-    };
 
-    return (
-        <form onSubmit={handleSubmit} id="add-to-cart-form">
-            <button type="submit" className="flex justify-center items-center w-60 p-2 border rounded-4xl shadow-md hover:bg-teal-50 hover:outline hover:cursor-pointer">
-                <img src={addToCart} className="w-10" />
-                <span className="text-2xl pl-5">Add to cart</span>
-            </button>
-            <ToastContainer />
-        </form>
-    )
+        localStorage.setItem("cart", JSON.stringify(cart));
+        toast("Item added to cart!", {
+          position: "bottom-center",
+          autoClose: 2000,
+        });
+      } catch (err) {
+        console.error("Error adding to local cart:", err);
+      }
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit} id="add-to-cart-form">
+      <button
+        type="submit"
+        className="flex w-60 items-center justify-center rounded-full border p-2 shadow-md hover:cursor-pointer hover:bg-teal-50 hover:outline max-lg:mb-5 max-lg:w-15"
+      >
+        <img src={addToCart} className="w-10" />
+        <span className="pl-5 text-2xl max-lg:hidden">Add to cart</span>
+      </button>
+      <ToastContainer />
+    </form>
+  );
 }
--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,81 +1,84 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { Helmet } from "react-helmet";
 import { BASE_URL } from "../components/utils/api";
 import Markdown from "react-markdown";
 import AddToCart from "../components/AddToCart";
 
 export default function SingleProduct() {
-    const { id } = useParams();
-    const [productInfos, setProductInfos] = useState([]);
+  const { id } = useParams();
+  const [productInfos, setProductInfos] = useState([]);
 
-    useEffect(() => {
-        window.scrollTo(0, 0); // This scrolls the page to the top when selecting a product from Products.jsx
-    }, [id]);
+  useEffect(() => {
+    window.scrollTo(0, 0); // This scrolls the page to the top when selecting a product from Products.jsx
+  }, [id]);
 
-    useEffect(() => {
-        fetch(`${BASE_URL}/products/${id}`)
-            .then(res => res.json())
-            .then(data => setProductInfos(data.product))
-            .catch(err => console.log(err))
-    }, [])
+  useEffect(() => {
+    fetch(`${BASE_URL}/products/${id}`)
+      .then((res) => res.json())
+      .then((data) => setProductInfos(data.product))
+      .catch((err) => console.log(err));
+  }, []);
 
-    return (
-        <div className="font-sans pt-[140px] px-[10%] bg-gray-100 mb-30">
-            {/* SECTION 1 */}
-            <div className="flex mb-10 shadow-lg rounded-xl">
-                <div className="inline-flex justify-center w-full bg-teal-50 rounded-xl">
-                    <img
-                        src={productInfos.image_url2}
-                        alt={productInfos.name}
-                        className="max-h-[80vh] w-[70%] object-contain bg-white rounded-l-xl"
-                    />
+  return (
+    <div className="mb-20 bg-gray-100 px-[10%] pt-[140px] max-2xl:px-[5%]">
+      <Helmet>
+        <title>{`${productInfos.name}`} | Guitar Shop</title>
+      </Helmet>
+      {/* SECTION 1 */}
+      <div className="mb-10 flex rounded-xl shadow-lg">
+        <div className="inline-flex w-full justify-center rounded-xl bg-teal-50">
+          <img
+            src={productInfos.image_url2}
+            alt={productInfos.name}
+            className="max-h-[75vh] w-[65%] rounded-l-xl bg-white object-contain px-[2%]"
+          />
 
-                    <div className="flex-1 w-[30%] pl-5 pt-15 border-l border-teal-200">
-                        <h1 className="text-4xl font-bold pb-5">{productInfos.brand}</h1>
-                        <h1 className="text-2xl pb-10">{productInfos.name}</h1>
+          <div className="w-[30%] flex-1 border-l border-teal-200 pt-15 pl-5 max-lg:pt-5">
+            <h1 className="pb-5 text-4xl font-bold">{productInfos.brand}</h1>
+            <h1 className="pb-10 text-2xl max-xl:pb-5">{productInfos.name}</h1>
 
-                        <div
-                            className={`text-xl my-2 pb-5 ${productInfos.stock > 0 ? 'text-green-600' : 'text-red-600'
-                                }`}
-                        >
-                            {productInfos.stock > 0 ? '● Available' : '● Out of Stock'}
-                        </div>
-
-                        <div className="text-4xl my-2 pb-10">
-                            ${Number(productInfos.price).toFixed(2)}
-                        </div>
-
-                        <AddToCart 
-                        product_id={productInfos.id} 
-                        brand={productInfos.brand} 
-                        name={productInfos.name} 
-                        image_url={productInfos.image_url} 
-                        price={productInfos.price} />
-                    </div>
-                </div>
+            <div
+              className={`my-2 pb-5 text-xl ${
+                productInfos.stock > 0 ? "text-green-600" : "text-red-600"
+              }`}
+            >
+              {productInfos.stock > 0 ? "● Available" : "● Out of Stock"}
             </div>
 
-            {/* SECTION 2 */}
-            <div className="flex flex-col items-center  my-24 mb-10 mx-auto leading-relaxed bg-teal-50 shadow-lg rounded-xl">
-                <h2 className="text-4xl font-semibold p-10 text-center ">Description</h2>
-                <div className="prose-lg px-[5%] py-5">
-                    <Markdown>{productInfos.description}</Markdown>
-                </div>
-                 <iframe
-                        src={productInfos.video_url}
-                        frameBorder="0"
-                        allowFullScreen
-                        className="w-[90%] h-200 my-5 mb-15 rounded-xl shadow-xl"
-                    ></iframe>
+            <div className="my-2 pb-10 text-4xl max-xl:pb-5">
+              ${Number(productInfos.price).toFixed(2)}
             </div>
 
-            {/* SECTION 3 */}
-            <div className="flex flex-col items-center w-full my-24">
-           
-                <div className="bg-teal-50 w-full flex justify-center border-x border-teal-400 bg-teal-50 shadow-lg">
-                   
-                </div>
-            </div>
+            <AddToCart
+              product_id={productInfos.id}
+              brand={productInfos.brand}
+              name={productInfos.name}
+              image_url={productInfos.image_url}
+              price={productInfos.price}
+            />
+          </div>
         </div>
-    )
+      </div>
+
+      {/* SECTION 2 */}
+      <div className="mx-auto my-24 mb-10 flex flex-col items-center rounded-xl bg-teal-50 leading-relaxed shadow-lg">
+        <h2 className="p-10 text-center text-4xl font-semibold">Description</h2>
+        <div className="prose-lg px-[5%] py-5">
+          <Markdown>{productInfos.description}</Markdown>
+        </div>
+        <iframe
+          src={productInfos.video_url}
+          frameBorder="0"
+          allowFullScreen
+          className="my-5 mb-15 aspect-16/9 w-[90%] rounded-xl shadow-xl"
+        ></iframe>
+      </div>
+
+      {/* SECTION 3 */}
+      <div className="my-24 flex w-full flex-col items-center">
+        <div className="flex w-full justify-center border-x border-teal-400 bg-teal-50 shadow-lg"></div>
+      </div>
+    </div>
+  );
 }
