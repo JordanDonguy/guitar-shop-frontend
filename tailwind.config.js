d27d057750b936766bc0d7fd1172988086ddd4bf
Jordan <jordan.donguy@gmail.com>
2025-05-14 22:52:36 +0200

    Install react-helmet in dependencies, and also prettier in dev dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,23 @@
 export default {
-    purge: [],
-    darkMode: false, // or 'media' or 'class'
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-      ],
-    theme: {
-      extend: {
-        fontFamily: {
-          onest: ['Onest', 'sans-serif'],
-        },
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        onest: ["Onest", "sans-serif"],
+      },
+      screens: {
+        "max-2xl": { raw: "(max-width: 1535px)" },
+        "max-xl": { raw: "(max-width: 1279px)" },
+        "max-lg": { raw: "(max-width: 1025px)" },
+        "max-md": { raw: "(max-width: 767px)" },
+        "max-sm": { raw: "(max-width: 639px)" },
       },
     },
-    variants: {
-      extend: {},
-    },
-    plugins: [require('@tailwindcss/typography')],
-  };
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
