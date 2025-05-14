8a5395b8194711a068037b1497de8255b51015de
Jordan <jordan.donguy@gmail.com>
2025-05-14 22:56:34 +0200

    Update the Routes in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,35 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import HomePage from './pages/HomePage';
-import Products from './pages/Products';
-import SingleProduct from './pages/SingleProduct';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import UserProfile from './pages/UserProfile';
-import Cart from './pages/Cart';
-import CheckoutForm from './pages/Checkout';
-import { AuthProvider } from './components/utils/AuthContext';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import HomePage from "./pages/HomePage";
+import Products from "./pages/Products";
+import SingleProduct from "./pages/SingleProduct";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import UserLayout from "./components/UserLayout";
+import UserProfile from "./pages/UserProfile";
+import Orders from "./pages/Orders";
+import Cart from "./pages/Cart";
+import CheckoutForm from "./pages/Checkout";
+import { AuthProvider } from "./components/utils/AuthContext";
 
 export default function App() {
-    return (
-        <AuthProvider>
-          <Routes>
-            <Route element={<Layout />}>
-              <Route index element={<HomePage />} />
-              <Route path="/products" element={<Products />} />
-              <Route path="/products/:id" element={<SingleProduct />} />
-              <Route path="/auth/login" element={<Login />} ></Route>
-              <Route path="/auth/register" element={<Register />}></Route>
-              <Route path="/user" element={<UserProfile />} ></Route>
-              <Route path="/cart" element={<Cart />} ></Route>
-              <Route path="/checkout" element={<CheckoutForm />}></Route>
-            </Route>
-          </Routes>
-        </AuthProvider>
-    )
-  };
+  return (
+    <AuthProvider>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<SingleProduct />} />
+          <Route path="/auth/login" element={<Login />}></Route>
+          <Route path="/auth/register" element={<Register />}></Route>
+          <Route path="/user" element={<UserLayout />}>
+            <Route path="/user/user-profile" element={<UserProfile />}></Route>
+            <Route path="/user/orders" element={<Orders />}></Route>
+          </Route>
+          <Route path="/cart" element={<Cart />}></Route>
+          <Route path="/checkout" element={<CheckoutForm />}></Route>
+        </Route>
+      </Routes>
+    </AuthProvider>
+  );
+}
