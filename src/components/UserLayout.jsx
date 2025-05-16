2da7684db15edf69fd07607a762f27c420837d29
Jordan <jordan.donguy@gmail.com>
2025-05-17 00:00:10 +0200

    Change the mobile orders and user profile buttons and add isActive visual feedback on links

--- src/components/UserLayout.jsx
+++ src/components/UserLayout.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Outlet, Link, useNavigate } from "react-router-dom";
+import { Outlet, NavLink, useNavigate } from "react-router-dom";
 import { useAuth } from "./utils/AuthContext";
 import { useMediaQuery } from "./utils/useMediaQuery";
 
@@ -8,29 +8,40 @@ const UserLayout = () => {
   const navigate = useNavigate();
   const isMobile = useMediaQuery("(max-width: 1024px)");
 
-    useEffect(() => {
-      if (loadingAuth) return;
-      if (!user) {
-        navigate("/auth/login");
-      }
-    }, [user, loadingAuth]);
+  useEffect(() => {
+    if (loadingAuth) return;
+    if (!user) {
+      navigate("/auth/login");
+    }
+  }, [user, loadingAuth, navigate]);
 
   if (isMobile) {
     return (
+
       <div className="fade-in mb-20 px-[5%] pt-[140px] flex flex-col">
-        <div className="mb-10 flex rounded-lg border-2 border-teal-600 p-1">
-          <Link
-            to="/user/user-profile"
-            className="w-1/2 border-r-2 border-teal-400 text-center text-2xl hover:text-neutral-500"
+        <div className="mb-10 flex overflow-hidden rounded-xl border-2 border-teal-600 shadow-md">
+          <NavLink
+            to="/user/profile"
+            className={({ isActive }) =>
+              `w-1/2 py-3 text-center text-xl font-medium transition ${isActive
+                ? "bg-teal-600 text-white"
+                : "bg-white  hover:bg-teal-100"
+              }`
+            }
           >
             User Profile
-          </Link>
-          <Link
+          </NavLink>
+          <NavLink
             to="/user/orders"
-            className="w-1/2 text-center text-2xl hover:text-neutral-500"
+            className={({ isActive }) =>
+              `w-1/2 py-3 text-center text-xl font-medium transition ${isActive
+                ? "bg-teal-600 text-white"
+                : "bg-white  hover:bg-teal-100"
+              }`
+            }
           >
             Orders
-          </Link>
+          </NavLink>
         </div>
         <Outlet />
       </div>
@@ -41,15 +52,15 @@ const UserLayout = () => {
   return (
     <div className="fade-in mb-30 flex min-h-screen px-[10%] pt-[140px]">
       <div className="sticky top-[140px] flex h-[80vh] w-1/6 flex-col border-r-2 border-neutral-400 max-2xl:w-1/4">
-        <Link
-          to="/user/user-profile"
-          className="mb-10 text-2xl hover:text-neutral-500"
+        <NavLink
+          to="/user/profile"
+          className={({ isActive }) => `mb-10 text-2xl hover:text-neutral-500 ${isActive ? "text-teal-500" : ""}`}
         >
           → User Profile
-        </Link>
-        <Link to="/user/orders" className="text-2xl hover:text-neutral-500">
+        </NavLink>
+        <NavLink to="/user/orders" className={({ isActive }) => `mb-10 text-2xl hover:text-neutral-500 ${isActive ? "text-teal-500" : ""}`}>
           → Orders
-        </Link>
+        </NavLink>
       </div>
       <Outlet />
     </div>
